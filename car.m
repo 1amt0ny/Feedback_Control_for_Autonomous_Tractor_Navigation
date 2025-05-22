cc75c092c6fb5e354af6f17c4c61080bb70c9e2c
Tony <48691420+1amt0ny@users.noreply.github.com>
2025-05-22 16:05:09 -0700

    Dubins Path Navigation in MATLAB

--- car.m
+++ car.m
@@ -1,4 +1,3 @@
-
 % The matlab script defines the ODE  
 % \dot{x} = v cos \theta
 % \dot{y} = v cos \theta
@@ -6,11 +5,16 @@
 %
 % The time varying u is passed to the function as a global variable
 %
-function dx=car(t,x)
-  global u;
-  global v;
-  theta=x(3,1);
-  dx(1,1)=v*cos(theta);
-  dx(2,1)=v*sin(theta);
-  dx(3,1)=u;
+% Kinematic Equations: including vehicle's Cartesian position x, y, and
+% orientation angle theta
+
+function dx=car(t,x)      % t: time; x: state vector
+  global w;               % turn rate
+  global u;               % velocity
+
+  phi=x(3,1);             % state vector (ẋ,̇y,̇φ)
+  
+  dx(1,1)=u*cos(phi);     % ẋ = ucos(φ)
+  dx(2,1)=u*sin(phi);     % ̇y = usin(φ)   (1)
+  dx(3,1)=w;              % ̇φ = ω
 end
--- unicycle.m
+++ unicycle.m
@@ -1,60 +1,92 @@
 clear all
 close all
-global u   % turning rate
-global v   % velocity
-v = 0; 
-u = 0;
-% Initial pose
-xInit=-2;yInit=1;thInit=135*pi/180;
-xD=xInit;yD=yInit;thD=thInit;
-% Parking point pose (headin of the pose is 0)
-xPose=0;yPose=0;thPose=0;
-% Emty arrays for storing the trajecory
-xDRec=[];yDRec=[];thDRec=[];
+global w   % turning rate
+global u   % velocity
+u = 0; 
+w = 0;
+
+% Initial pose (x_0,y_0,φ_0)
+x_Init=0;y_Init=1;phi_Init=90*pi/180;
+
+% Current pose (initialized as initial pose)
+x_D=x_Init;y_D=y_Init;phi_D=phi_Init;
+
+% Parking point pose (x,y,φ)
+%x_Pose=0;y_Pose=-1;phi_Pose=0*pi/180;
+x_Pose=3;y_Pose=1;phi_Pose=90*pi/180;
+
+% Empty arrays for storing the trajecory
+xDRec=[];yDRec=[];phiDRec=[];
+
 % Initial distance 
-r = sqrt((xPose-xD)^2+(yPose-yD)^2);
-while (r > 0.001),
-    phi=thD-thPose;
-    theta=atan2(yPose-yD,xPose-xD)-thPose;
+e = sqrt((x_Pose-x_D)^2+(y_Pose-y_D)^2);
+
+while (e > 0.001)
+    
+    % relative orientation of the car with respect to the target pose: 
+    phi=phi_D-phi_Pose;
+
+    % the angle to the target pose from the car's current position:
+    theta=atan2(y_Pose-y_D,x_Pose-x_D)-phi_Pose;
+
+    % normalize θ to be within [-π,π]:
     theta=atan2(sin(theta),cos(theta));
+
+    % the angle between the car's current orientation and the direction towards the target pose:
     alpha=theta-phi;
+    
+    % normalize α to be within [-π,π]:
     alpha=atan2(sin(alpha),cos(alpha));
+
     % distance 
-    r = sqrt((xPose-xD)^2+(yPose-yD)^2);
+    e = sqrt((x_Pose-x_D)^2+(y_Pose-y_D)^2);
+
     k=6.0;      %control parameter
     gamma=3.0;  %control parameter
-    h=3.0;      %control parameter
+    h=1.0;      %control parameter
+    % h=3.0;
+
     % turning rate control
-    u=k*alpha+gamma*cos(alpha)*sin(alpha)/alpha*(alpha+h*theta);
+    w=k*alpha+gamma*cos(alpha)*sin(alpha)/alpha*(alpha+h*theta); 
+                            % ω = kα+γ[cos(α)sin(α)/α](α+hθ); k > 0 (9)
+
     % velocity control
-    v=gamma*cos(alpha)*r;
+    u=gamma*cos(alpha)*e;   % u = (γcos(α)e; γ > 0 (6)
+
     % saturation of the velocity reference
-    if (v > 0.5),
-        v=0.5;
-    elseif (v<-0.5),
-        v=-0.5;
+    if (u > 1)
+        u=1;
+    elseif (u<-1)
+        u=-1;
     end
+
     % saturation of the turning rate reference
-    if (u > 2), 
-        u = 2;
-    elseif (u < -2),
-        u=-2;
+    if (w > 2) 
+        w = 2;
+    elseif (w < -2)
+        w=-2;
     end
-    % compute dynamisc for 50ms using (xD, yD, thD) as the initial state
-    [t sol]= ode45(@car,[0 0.05],[xD;yD;thD]);
-    % Store the solution
+
+    % compute dynamics for 50ms using (xD, yD, thD) as the initial state
+    [t, sol]= ode45(@car,[0 0.05],[x_D;y_D;phi_D]);
+    % [0 0.05] specifies the time span for the ODE solver (50ms duration)
+    % [x_D; y_D; phi_D] = current state vector (ẋ,̇y,̇φ)
+
+    % Store the solution trajectory
     xDRec=[xDRec sol(end,1)];
     yDRec=[yDRec sol(end,2)];
-    thDRec=[thDRec sol(end,3)];
+    phiDRec=[phiDRec sol(end,3)];
+
     % Initial pose for the next pass
-    xD=sol(end,1);yD=sol(end,2);thD=sol(end,3);    
+    x_D=sol(end,1);y_D=sol(end,2);phi_D=sol(end,3);  
+
     % Plots the car and the trajectory
     figure(1)
-    plotcar(xDRec(1),yDRec(1),thDRec(1),0.2)
+    plotcar(xDRec(1),yDRec(1),phiDRec(1),0.2)
     axis([-2 2 -2 2]);
     axis equal     
     plot(xDRec,yDRec,'k','LineWidth',2), hold on;
-    plotcar(xD,yD,thD,0.2)
+    plotcar(x_D,y_D,phi_D,0.2)
     figure(1),hold off
 end    
    
